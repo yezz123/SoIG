086977c5ad89dc2d459e931da98997cca62ced6d
Yasser Tahiri <yasserth19@gmail.com>
2021-07-18 20:53:08 +0100

    🛠 Fix the api and the Main file

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ line="\e[1;31m[*]\e[0m"
 echo -e "$line Starting Install ..."
 echo -e "$line This May Take Some Time"
 chmod 777 main.py
-command -v python > /dev/null 2>&1 || apt install -y python
-command -v python3 > /dev/null 2>&1 || apt install -y python
+command -v python >/dev/null 2>&1 || apt install -y python
+command -v python3 >/dev/null 2>&1 || apt install -y python
 pip3 install -r requirements.txt
 python3 -m pip install -r requirements.txt
 echo -e "$line Install Complete"
